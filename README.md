db896d65008944671672d44a455c4b9a40adf157
Martin <septum1@yandex.ru>
2022-08-09 12:43:40 +0700

    add H2 db

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # java-filmorate
 Template repository for Filmorate project.
+
+<picture>
+Схема базы данных
+ <img alt="Show database" src=src/main/resources/schema.png>
+</picture>
+
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+MERGE INTO "FILM_GENRES" ("GENRES_ID", "FILM_GENRE") VALUES (1, 'Комедия'), (2, 'Драма'), (3, 'Мультфильм'), (4, 'Триллер'), (5, 'Документальный'), (6, 'Боевик');
+MERGE INTO "FILM_RATING_MPA" ("MPA_ID", "MPA_RATE") VALUES (1, 'G'), (2, 'PG'), (3, 'PG-13'), (4, 'R'), (5, 'NC-17');
